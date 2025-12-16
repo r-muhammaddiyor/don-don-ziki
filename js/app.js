0615fe9c542c85500d96ccd3d4ef8f6d9d90adb6
muhammaddiyor <muhammaddiyor@gmail.com>
2025-12-17 02:10:41 +0500

    ignore modules

--- js/app.js
+++ js/app.js
@@ -1,16 +1,54 @@
 import {
+  elAdvansed,
   elAi,
+  elBasic,
   elGameZone,
   elHands,
   elRefreshGameButton,
   elResultZone,
   elUser,
+  elModeChangerButton,
+  elFindWinner,
+  elScore,
 } from './html-elements.js';
+
+// rules
+
+const elRulesImage = document.querySelector('#rulesImage');
+
+
+let activeMode = 'advanced';
+
+function modeChanger() {
+  if (activeMode === 'advanced') {
+    activeMode = 'basic';
+
+    elAdvansed.classList.add('hidden');
+    elBasic.classList.remove('hidden');
+    elBasic.classList.add('grid');
+    elRulesImage.src = '../images/rule-basic.svg';
+
+    elModeChangerButton.innerText = 'ADVANCED';
+  } else {
+    activeMode = 'advanced';
+
+    elBasic.classList.add('hidden');
+    elBasic.classList.remove('grid');
+
+    elAdvansed.classList.remove('hidden');
+    elRulesImage.src = '../images/rule-advanced.svg';
+
+    elModeChangerButton.innerText = 'BASIC';
+  }
+}
+
+
+
 
 // AI choose
 
 function aiChoose() {
-  const hands = ['rock', 'paper', 'scissors'];
+  const hands = ['rock', 'paper', 'scissors', ];
   const randomIndex = Math.trunc(Math.random() * hands.length);
   return hands[randomIndex];
 }
@@ -27,28 +65,66 @@ function swapZone(showResult) {
   }
 }
 
+elRefreshGameButton.addEventListener('click', () => {
+  swapZone(false);
+  elFindWinner.innerText = '';
+});
+
+
 // Find winner
 
 function checkWinner(user, ai) {
-  if(user === ai) {
-    return "TIE"
-  } else if(user === "paper" && ai === "scissors") {
-    return "ROBOT";
-  } else if (user === 'scissors' && ai === 'rock') {
-    return 'ROBOT';
-  } else if (user === 'rock' && ai === 'paper') {
-    return 'ROBOT';
+  const actions = {
+    paper: {
+      scissors: 'AI',
+      rock: 'USER',
+      spock: 'USER',
+      lizart: 'AI',
+    },
+    scissors: {
+      paper: 'USER',
+      rock: 'AI',
+      spock: 'AI',
+      lizart: 'USER',
+    },
+    rock: {
+      paper: 'AI',
+      scissors: 'USER',
+      spock: 'AI',
+      lizart: 'USER',
+    },
+    spock: {
+      scissors: 'USER',
+      rock: 'USER',
+      paper: 'AI',
+      lizart: 'AI',
+    },
+    lizard: {
+      scissors: 'AI',
+      rock: 'AI',
+      paper: 'USER',
+      spock: 'USER',
+    },
+  };
+
+  if (user === ai) {
+    return "TIE";
   } else {
-    return "USER"
+    return actions[user][ai];
   }
 }
 
+const winShadow = 'shadow-[0_0_0_25px_#fff2]';
 
 elResultZone.classList.add('hidden');
 
 elHands.forEach((elHand) => {
   elHand.addEventListener('click', (evt) => {
     swapZone(true);
+
+    elUser.classList.remove(winShadow)
+    elAi.classList.remove(winShadow)
+
     const user = evt.target.alt;
     const ai = aiChoose();
 
@@ -56,8 +132,21 @@ elHands.forEach((elHand) => {
     elAi.src = './images/choosing.svg';
     setTimeout(() => {
       elAi.src = `./images/${ai}.svg`;
-      const winner = checkWinner(user, ai)
-      console.log(winner);
+      const winner = checkWinner(user, ai);
+
+      if (winner === 'USER') {
+        elFindWinner.innerText = 'YOU WIN';
+        elUser.classList.add('shadow-[0_0_0_25px_#fff2]');
+        changeScore()
+      } else if (winner === 'AI') {
+        elFindWinner.innerText = 'YOU LOSE';
+        elAi.classList.add('shadow-[0_0_0_25px_#fff2]');
+      } else {
+        elFindWinner.innerText = 'TIE';
+        elUser.classList.remove('shadow-[0_0_0_25px_#fff2]');
+        elAi.classList.remove('shadow-[0_0_0_25px_#fff2]');
+      }
+
     }, 1000);
   });
 });
@@ -67,3 +156,19 @@ elHands.forEach((elHand) => {
 elRefreshGameButton.addEventListener('click', () => {
   swapZone(false);
 });
+
+
+// Change score
+
+function changeScore() {
+  elScore.innerText = +elScore.innerText + 1;
+  
+}
+
+
+// Change mode
+elModeChangerButton.addEventListener("click", modeChanger)
+
+// Start
+
+modeChanger()
--- js/html-elements.js
+++ js/html-elements.js
@@ -5,5 +5,21 @@ const elRefreshGameButton = document.querySelector('#refreshGameButton');
 const elGameZone = document.querySelector('#gameZone');
 const elResultZone = document.querySelector('#resultZone');
 const elFindWinner = document.querySelector('#findWinner');
+const elModeChangerButton = document.querySelector('#modeChanger');
+const elBasic = document.querySelector('#basic');
+const elAdvansed = document.querySelector('#advansed');
+const elScore = document.querySelector('#score');
 
-export { elHands, elUser, elAi, elRefreshGameButton, elGameZone, elResultZone };
+export {
+  elHands,
+  elUser,
+  elAi,
+  elRefreshGameButton,
+  elGameZone,
+  elResultZone,
+  elModeChangerButton,
+  elBasic,
+  elFindWinner,
+  elAdvansed,
+  elScore
+};
